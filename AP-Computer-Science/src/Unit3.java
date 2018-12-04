e62373c219e3c65f996a4586593ba059e99af0bc
Tie C <usbtyping@gmail.com>
2018-12-04 15:47:51 -0800

    Ex 7.7

--- AP-Computer-Science/src/Unit3.java
+++ AP-Computer-Science/src/Unit3.java
@@ -2,7 +2,6 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintStream;
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
@@ -12,7 +11,7 @@ public class Unit3 {
 
 	public static void main(String[] args) throws IOException {
 		//exercise3();
-		exercise7_5();
+		exercise7_7();
 		//arrayPopulation();
 		//arrayTable();
 	}
@@ -124,6 +123,7 @@ public class Unit3 {
 		}
 	}
 	
+	@SuppressWarnings("resource")
 	public static void exercise7_1(){
 		while(true) {
 			Scanner input = new Scanner(System.in);
@@ -141,6 +141,7 @@ public class Unit3 {
 		}
 	}
 	
+	@SuppressWarnings("resource")
 	public static void exercise7_2() {
 		while(true) {
 			Scanner input = new Scanner(System.in);
@@ -170,6 +171,7 @@ public class Unit3 {
 		}
 	}
 	
+	@SuppressWarnings("resource")
 	public static void exercise7_3(){
 		Scanner input = new Scanner(System.in);
 		String in = input.next();
@@ -244,8 +246,45 @@ public class Unit3 {
 		return cachearr;
 	}
 	
+	@SuppressWarnings("resource")
 	public static void exercise7_6() {
-		
+		System.out.println("Enter Quantity of Numbers:");
+		Scanner qinput = new Scanner(System.in);
+		int[] nums = new int[qinput.nextInt()];
+		int[] adds = new int[nums.length-1];
+		System.out.println("Input numbers with a , between each number:");
+		Scanner cinput = new Scanner(System.in);
+		Scanner ninput = new Scanner(cinput.next()).useDelimiter(",");
+		int pinput = 0;
+		int input = 0;
+		int count = 0;
+		for(int i = 0; i < nums.length; i++) {
+			nums[i] = ninput.nextInt();
+			count++;
+		}
+		for(int i = 0; i<adds.length; i++) {
+			adds[i] = nums[i] + nums[i+1];
+		}
+		System.out.println(Arrays.toString(adds));
+	}
+	
+	public static void exercise7_7() {
+		int [] one = {1,2,3,4,5};
+		int [] two = {6,7,8,9,10};
+		System.out.println(Arrays.toString(append(one,two)));
+	}
+	
+	public static int[] append(int [] a, int [] b) {
+		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(b));
+		int[] app = new int[a.length+b.length];
+		for(int i = 0; i < a.length; i++) {
+			app[i] = a[i];
+		}
+		for(int i = a.length; i < b.length+a.length; i++) {
+			app[i] = b[i - a.length];
+		}
+		return app;
 	}
 }
 
