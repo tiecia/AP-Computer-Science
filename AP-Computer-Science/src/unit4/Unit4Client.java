4e012e481586667edf2c8bc822a792dc7cb663f9
Tie C <usbtyping@gmail.com>
2019-01-08 19:27:19 -0800

    Work on Ex

--- AP-Computer-Science/src/unit4/Unit4Client.java
+++ AP-Computer-Science/src/unit4/Unit4Client.java
@@ -6,37 +6,40 @@ public class Unit4Client {
 		Point p1 = new Point();
 		Point p2 = new Point();
 		
-		p1.x = 2;
-		p1.y = 2;
+		p1.setX(2);
+		p1.setY(2);
 		
-		p2.x = 4;
-		p2.y = 4;
+		p2.setX(4);
+		p2.setY(4);
 		
 		System.out.println(findSlope(p1,p2));
 		System.out.println(findDistance(p1,p2));
 		System.out.println(findMidpoint(p1,p2));
 		
 		Triangle t = new Triangle();
+		
 		t.p1 = p1;
 		t.p2 = p2;
-		t.p3.x = 6;
-		t.p3.y  = 6;
+		t.p3.setX(6);
+		t.p3.setY(6);
 		
 		System.out.println();
 		System.out.println(findPerm(t));
 		
+		System.out.println(p1.distanceTo(p2));
+		
 	}
 	
 	public static String findSlope(Point p1, Point p2) {
-		return (p2.y - p1.y) + "/" + (p2.x - p1.x);
+		return (p2.getY() - p1.getY()) + "/" + (p2.getX() - p1.getX());
 	}
 	
 	public static double findDistance(Point p1, Point p2) {
-		return (Math.sqrt((p2.x - p1.x)^2 + (p2.y - p1.y)^2));
+		return (Math.sqrt((p2.getX() - p1.getX())^2 + (p2.getY() - p1.getY())^2));
 	}
 	
 	public static String findMidpoint(Point p1, Point p2) {
-		return (p1.x + p2.x)/2 + "," + (p1.y + p2.y)/2;
+		return (p1.getX() + p2.getX())/2 + "," + (p1.getY() + p2.getY())/2;
 	}
 	
 	public static double findPerm(Triangle t) {
