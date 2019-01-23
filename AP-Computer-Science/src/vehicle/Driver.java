145dfea1773056544e353f6cb7ca2932bd2e4376
Tie C <usbtyping@gmail.com>
2019-01-22 17:47:54 -0800

    Work on Ex1

---
+++ AP-Computer-Science/src/vehicle/Driver.java
@@ -0,0 +1,7 @@
+package vehicle;
+
+public class Driver {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+}
---
+++ AP-Computer-Science/src/vehicle/Vehicle.java
@@ -0,0 +1,21 @@
+package vehicle;
+
+public class Vehicle extends Driver{
+	
+	public void start() {
+		
+	}
+	
+	public void stop() {
+		
+	}
+	
+	public void turn() {
+		
+	}
+	
+	public void getSpeed() {
+		
+	}
+
+}
