318dfd155a3605742e24ad9fb43462870c6cc5bc
Tie C <usbtyping@gmail.com>
2019-03-04 11:25:52 -0800

    ex3

--- AP-Computer-Science/src/Unit6.java
+++ AP-Computer-Science/src/Unit6.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class Unit6 {
 
 	public static void main(String[] args) {
-		exercise2();
+		exercise3();
 	}
 	
 	public static void exercise1() {
@@ -66,11 +66,30 @@ public class Unit6 {
 	}
 	
 	public static void exercise2() {
+		ArrayList<String> sList = new ArrayList<String>();
+		sList.add(0, "String");
+		sList.add(1, "Strings");
+		sList.add(2, "Integer");
+		sList.add(3, "Integers");
+		
 		ArrayList<Double> list= new ArrayList<Double>();
 		for(double i = 0.5; i<10; i++) {
 			list.add(i);
 		}
+		
+		removeEvenLength(sList);
 		doubleList(list);
+	}
+	
+	public static void removeEvenLength(ArrayList<String> list) {
+		System.out.println(list);
+		for(int i = 0; i<list.size(); i++) {
+			if(list.get(i).length() % 2 == 0) {
+				list.remove(i);
+				i--;
+			}
+		}
+		System.out.println(list);
 	}
 	
 	public static void doubleList(ArrayList<Double> list) {
@@ -79,6 +98,29 @@ public class Unit6 {
 			list.add(i+1, list.get(i));
 		}
 		System.out.println(list);
+	}
+	
+	public static void exercise3() {
+		ArrayList<Integer> list = new ArrayList<Integer>();
+		for(int i = 0; i<30; i++) {
+			list.add(i);
+		}
+		System.out.println(list);
+		reverse(list);
+		System.out.println(list);
+	}
+	
+	public static ArrayList reverse(ArrayList<Integer> list) {
+		for(int i = 0; i<list.size(); i+=3) {
+			try{
+				Integer cache = list.get(i);
+				list.set(i, list.get(i+2));
+				list.set(i+2, cache);
+			} catch(IndexOutOfBoundsException e) {
+				i = list.size()+1;
+			}
+		}
+		return list;
 	}
 
 }
