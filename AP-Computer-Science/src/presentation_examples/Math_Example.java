bd48c9bb36f16bdf4faa1f68725a9881c5d11918
Tie C <usbtyping@gmail.com>
2019-06-03 10:38:31 -0700

    Examples for Presentation

---
+++ AP-Computer-Science/src/presentation_examples/Math_Example.java
@@ -0,0 +1,12 @@
+package presentation_examples;
+
+public class Math_Example {
+	public static void main(String[] args) {
+		System.out.println(Math.abs(-20));
+		//No factorial method in java Math class
+		//No GCD method in the java Math class
+		System.out.println(Math.pow(10, 10));
+		System.out.println(Math.sqrt(200));
+
+	}
+}
